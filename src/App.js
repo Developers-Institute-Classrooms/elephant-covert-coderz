d26b23b580108b0a5e132b81efee3f5bb11559d7
mark churcher <mark@developersinstitute.ac.nz>
2020-09-30 13:20:07 +1300

    add input type

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Covert Coderz Killer Tax Calculator
-        </p>
-        <p>
-          Deployed with CI magic!!
-        </p>
-        <label>Number of purchased items</label>
-       <input type='number'/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>
+                Deployed with CI magic!!
+            </p>
+            <div>
+                <label>Number of purchased items:</label>
+                <input type='number' />
+            </div>
+            <div>
+                <label>Price of your items:</label>
+                <input type='number' />
+            </div>
+        </div>
+
+
+
+    );
 }
 
 export default App;
